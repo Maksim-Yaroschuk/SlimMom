b36116811f6bc478a4378eb38a640d7ec8f75447
Mykola Masiuk <masiuk.mykola@gmail.com>
2022-12-17 14:57:11 +0200

    added redux and onclick on exit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,24 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Home } from 'pages/Home';
-import { Header } from './Header/Header';
 import { Box } from './Box';
 import { DiaryPage } from 'pages/DiaryPage';
 import { CalculatorPage } from 'pages/CalculatorPage';
+import { Layout } from './Layout/Layout';
+import { LoginPage } from 'pages/LoginPage';
+import { RegisterPage } from 'pages/RegisterPage';
 
 export const App = () => {
   return (
     <Box>
-      <Header />
-
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/diary" element={<DiaryPage />} />
-        <Route path="/calculator" element={<CalculatorPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="home" />} />
+          <Route path="home" element={<Home />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+          <Route path="diary" element={<DiaryPage />} />
+          <Route path="calculator" element={<CalculatorPage />} />
+        </Route>
       </Routes>
     </Box>
   );
---
+++ src/redux/store.js
@@ -0,0 +1,36 @@
+import authReducer from './authSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['token'],
+};
+
+export const store = configureStore({
+  reducer: {
+    auth: persistReducer(persistConfig, authReducer),
+  },
+
+  middleware: getDefaultMiddleware => [
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  ],
+});
+
+export const persistor = persistStore(store);
