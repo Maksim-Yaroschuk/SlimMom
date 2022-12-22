904882cc027accf14888fc04c2c1adc6d00b38f1
zhekichaan <eugene.chernitskiy@gmail.com>
2022-12-21 20:26:48 -0500

    update

--- src/pages/CalculatorPage.jsx
+++ src/pages/CalculatorPage.jsx
@@ -8,16 +8,19 @@ import {
 import { ThemeContext } from 'components/Context/Context';
 import { useContext } from 'react';
 import Snowfall from 'react-snowfall';
+import { getUserInfo } from 'redux/authSelectors';
+import { useSelector } from 'react-redux';
 
 const CalculatorPage = () => {
   const { isChristmas } = useContext(ThemeContext);
+  const userInfo = useSelector(getUserInfo)
 
   return (
     <CalculatorPageWrapper>
       {isChristmas && <Snowfall />}
       <Box maxWidth={'1280px'} m={'0 auto'}>
         <H2>Calculate your daily calorie intake right now</H2>
-        <WeightForm />
+        <WeightForm initialValues={userInfo}/>
       </Box>
     </CalculatorPageWrapper>
   );
--- src/redux/authSelectors.js
+++ src/redux/authSelectors.js
@@ -1,3 +1,4 @@
 export const getIsLoggedIn = state => state.auth.isLogged;
 export const getUserName = state => state.auth.user.name;
 export const getToken = state => state.auth.token;
+export const getUserInfo = state => state.auth.userInfo;
