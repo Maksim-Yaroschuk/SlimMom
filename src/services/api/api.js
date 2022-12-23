9fe025986a40d844523df0f4c151c42eaa80eec2
unknown <4ndrey.tkachenko@gmail.com>
2022-12-23 13:54:24 +0200

    Update api.js

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,15 +1,12 @@
 import axios from 'axios';
 axios.defaults.baseURL = 'https://slimmom-oz0k.onrender.com';
-// const API_KEY = 'e8938dc969e4885481c1163e17374d0f';
+// const API_KEY = '';
 
 const END_POINTS = {
   DailyCalorieIntake: '/api/products',
 };
 
 export const apiCalorieIntake = async body => {
-  // if (body) {
   const res = await axios.post(END_POINTS.DailyCalorieIntake, body);
-  // console.log('resData', res.data);
   return res.data;
-  // }
 };
