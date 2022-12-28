f44fd00ba3356b4832e2e6accbd14607e321fab0
Mykola Masiuk <masiuk.mykola@gmail.com>
2022-12-28 13:05:04 +0200

    fixed buttons

--- src/components/ExitModal/ExitModal.jsx
+++ src/components/ExitModal/ExitModal.jsx
@@ -3,10 +3,13 @@ import { ButtonWrapper } from 'components/Form/Form.styled';
 import { InnerInfo, ModalWindow, Overlay } from 'components/Modal/Modal.styled';
 import React from 'react';
 import { createPortal } from 'react-dom';
+import { useMediaQuery } from 'react-responsive';
 
 const modalRoot = document.querySelector('#modal-root');
 
 export const ExitModal = ({ onClose, handleLogout }) => {
+  const isMobile = useMediaQuery({ query: '(max-width: 426px)' });
+
   const handleBackDropClick = e => {
     if (e.currentTarget === e.target) {
       onClose(false);
@@ -16,15 +19,21 @@ export const ExitModal = ({ onClose, handleLogout }) => {
     <Overlay onClick={handleBackDropClick}>
       <ModalWindow style={{ paddingTop: '50px' }}>
         <InnerInfo>
-          <h2>Are you sure you want to log out?</h2>
+          <h2 style={{ textAlign: 'center' }}>
+            Are you sure you want to log out?
+          </h2>
           <img
             src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/43d5c285197371.5d7503eab20c1.gif"
             alt="cat"
-            height="400"
+            height={isMobile ? '300' : '400'}
           />
         </InnerInfo>
         <ButtonWrapper
-          style={{ display: 'flex', gap: '20px', position: 'static' }}
+          style={
+            isMobile
+              ? { display: 'flex', flexWrap: 'wrap', gap: '20px' }
+              : { display: 'flex', gap: '20px', position: 'static' }
+          }
         >
           <div onClick={onClose}>
             <Button full={true}>No</Button>
